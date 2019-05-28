e75331a57e2f544883b4e1fd987cb9194bb01ccb
KongYue <haozaici@gmail.com>
2019-05-28 17:08:48 +1000

    The firefox and chrome show different

---
+++ js/scripts.js
@@ -0,0 +1,152 @@
+const alert = document.getElementById("alert");
+alert.innerHTML =
+  '<div class="alert-banner"><p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p><p class="alert-banner-close">x</p></div>';
+alert.addEventListener("click", e => {
+  const element = e.target;
+  if (element.classList.contains("alert-banner-close")) {
+    alert.style.display = "none";
+  }
+});
+
+let trafficData = {
+  labels: [
+    "16-22",
+    "23-29",
+    "30-5",
+    "6-12",
+    "13-19",
+    "20-26",
+    "27-3",
+    "4-10",
+    "11-17",
+    "18-24",
+    "25-31"
+  ],
+  datasets: [
+    {
+      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+      backgroundColor: "rgba(116, 119, 191, .3)",
+      borderWidth: 1
+    }
+  ]
+};
+
+let trafficOptions = {
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0
+  },
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true
+        }
+      }
+    ]
+  },
+  legend: {
+    display: false
+  }
+};
+
+var ctx = document.getElementById("traffic-chart");
+var myChart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: "line",
+
+  // The data for our dataset
+  data: trafficData,
+
+  // Configuration options go here
+  options: trafficOptions
+});
+
+const dailyCanvas = document.getElementById("daily-chart");
+
+// data for daily traffic bar chart
+const dailyData = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [
+    {
+      label: "# of Hits",
+      data: [75, 115, 175, 125, 225, 200, 100],
+      backgroundColor: "#7477BF",
+      borderWidth: 1
+    }
+  ]
+};
+const dailyOptions = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true
+        }
+      }
+    ]
+  },
+  legend: {
+    display: false
+  }
+};
+
+let dailyChart = new Chart(dailyCanvas, {
+  type: "bar",
+  data: dailyData,
+  options: dailyOptions
+});
+
+const mobileCanvas = document.getElementById("mobileusers-chart");
+const mobileData = {
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [
+    {
+      label: "# of Users",
+      data: [2000, 550, 500],
+      borderWidth: 0,
+      backgroundColor: ["#7477BF", "#78CF82", "#51B6C8"]
+    }
+  ]
+};
+
+const mobileOptions = {
+  legend: {
+    position: "right",
+    labels: {
+      boxWidth: 20,
+      fontStyle: "bold"
+    }
+  },
+  layout: {
+    padding: {
+      left: 50,
+      right: 0,
+      top: 0,
+      bottom: 0
+    }
+  }
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+  type: "doughnut",
+  data: mobileData,
+  options: mobileOptions
+});
+
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener("click", () => {
+  // ensure user and message fields are filled out
+  if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+  } else if (user.value === "") {
+    alert("Please fill out user field before sending");
+  } else if (message.value === "") {
+    alert("Please fill out message field before sending");
+  } else {
+    alert(`Message successfully sent to: ${user.value}`);
+  }
+});
